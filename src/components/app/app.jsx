7a64061795362a0c1e5d78b3ef147ce8d9a65483
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-07-15 19:55:06 +0300

    feat: add PropTypes of data

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,20 @@
-import styles from "./app.module.css";
-import { data, ingredientsTypes } from "../../utils/data";
+import styles from './app.module.css';
+import { data, ingredientsTypes } from '../../utils/data';
+import { ingredientPropType, ingredientsPropType } from '../../utils/prop-types';
+import PropTypes from "prop-types";
 
 import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+
+BurgerIngredients.propTypes = {
+  ingredients: PropTypes.arrayOf(ingredientPropType).isRequired,
+  ingredientsTypes: ingredientsPropType.isRequired
+};
+
+BurgerConstructor.propTypes = {
+  ingredients: PropTypes.arrayOf(ingredientPropType).isRequired,
+};
 
 function App() {
   return (
