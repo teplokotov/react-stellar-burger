f16ada3cd6623a268186f298fb54f2f1561d7199
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-07-25 20:57:50 +0300

    fix: add image sizes for Order popup

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -6,7 +6,7 @@ function OrderDetails() {
     <section className={`${orderDetailsStyles.section} pt-4 pb-15`} aria-label='Информация о заказе'>
       <p className={`${orderDetailsStyles.id} text text_type_digits-large`}>034536</p>
       <p className={`${orderDetailsStyles.description} text text_type_main-medium pt-8`}>идентификатор заказа</p>
-      <img className="pt-15 pb-15" src={imgDone} alt="Готово" />
+      <img className={`${orderDetailsStyles.image} pt-15 pb-15`} src={imgDone} alt="Готово" />
       <p className={`${orderDetailsStyles.status} text text_type_main-default`}>Ваш заказ начали готовить</p>
       <p className={`${orderDetailsStyles.waitingArea} text text_type_main-default text_color_inactive pt-2`}>Дождитесь готовности на орбитальной станции</p>
     </section>
